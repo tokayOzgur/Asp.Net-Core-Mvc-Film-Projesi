ac1423b9cbcf687bb658758ff8f5e9de1a6c8ac7
Özgür Tokay <ozytky@gmail.com>
2021-07-27 15:55:32 +0300

    ++

--- MovieApp/Startup.cs
+++ MovieApp/Startup.cs
@@ -17,7 +17,10 @@ namespace MovieApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            services.AddOptions();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,13 +33,13 @@ namespace MovieApp
 
             app.UseRouting();
 
-           /*  app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            }); */
+            /*  app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapGet("/", async context =>
+                 {
+                     await context.Response.WriteAsync("Hello World!");
+                 });
+             }); */
 
             app.UseMvcWithDefaultRoute();
         }
